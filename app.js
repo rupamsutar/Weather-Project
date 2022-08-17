214d14b5b26943e562da2ed28dee67ef1779ecd0
Rupam Sutar <rupamsutar281998@gmail.com>
2022-08-17 15:49:57 +0530

    https get request from external server added

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
+const https = require("https");
 
 app.get("/", function(req,res) {
+
+  const url = "https://api.openweathermap.org/data/2.5/weather?lat=18.516726&lon=73.856255&appid=779ed2fa86a431065411d589ceae3c46"
+  https.get(url, function(response) {
+    console.log(response);
+  });
   res.send("Server is Up and running !")
 })
 
