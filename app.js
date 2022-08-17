b57b362c41d8daf5da8daf87b01a6cabe2a91d27
Rupam Sutar <rupamsutar281998@gmail.com>
2022-08-17 19:18:33 +0530

    body parser added
    html added and form added in that html
    data is collected as post from the root page
    and displayed the result

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
 const https = require("https");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req,res) {
 
-  const query = "London";
+  res.sendFile(__dirname + "/index.html");
+
+});
+
+app.post("/", function(req,res) {
+
+  const query = req.body.cityName;
   const apiKey = "779ed2fa86a431065411d589ceae3c46";
   const unit = "metric"
   const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
@@ -17,12 +26,18 @@ app.get("/", function(req,res) {
       const icon = weatherData.weather[0].icon;
       const imageURL = "https://openweathermap.org/img/wn/" + icon +"@2x.png";
       res.write("<p> The weather currently is " + weatherDescription + "!</p>");
-      res.write("<h1>The temperature of Pune is " + temp + " degree Celcius !</h1>");
+      res.write("<h1>The temperature of "+ query + " is " + temp + " degree Celcius !</h1>");
       res.write("<img src = " + imageURL + ">");
       res.send();
     });
   });
 });
+
+
+
+
+
+
 
 app.listen(3000, function() {
   console.log("Server is running on port 3000");
